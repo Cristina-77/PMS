d66f91d15986c823515599951f38ad737eebe08d
bogdy <ybm2004@gmail.com>
2025-06-27 03:30:19 +0300

    Delete PMS_watch directory

--- PMS_watch/app/src/main/java/com/patientmonitoringsystem/presentation/theme/Typography.kt
+++
@@ -1,9 +0,0 @@
-package com.patientmonitoringsystem.presentation.theme
-
-import androidx.compose.material.Typography
-import androidx.compose.ui.text.font.FontFamily
-
-// Poți adăuga fonturi custom aici
-val Typography = Typography(
-    defaultFontFamily = FontFamily.Default
-)
